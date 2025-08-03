6b49a2104aab1af9f155669912538e20b8ff87ea
Tasbeeha Khan <tasbeehakhan05@gmail.com>
2025-08-04 01:23:15 +0500

    Initial commit for score game

---
+++ script.js
@@ -0,0 +1,44 @@
+// ternary operator
+
+// let age = 15;
+
+// let result = age >=18 ? "adult" : "not adult";
+// console.log(result);
+
+
+
+// prompt
+
+
+// let name = prompt("enter your name here");
+// console.log(name) ;
+
+
+
+// let num = prompt ("enter a number")
+
+// if( num % 5 === 0) {
+//     console.log( num, "5 is multiply of 5");
+// } else{
+//     console.log(num, "is not divisible by 5" );
+// }
+
+
+// let num = prompt ("enter a number  (0-100")
+
+// let grades;
+
+// if(num  >= 90 &&  num <= 100 ) {
+// grades = "A";
+// }else if (num  >= 70 && num <=89 ){
+// grades ="B";
+// }else if( num  >= 60 && num <=69 ){
+//  grades ="c";
+// }else if (num  >= 50 && num <=59){
+//    grades ="d";
+// } else if (num  >= 40 && num <=49){
+//     grades ="f";
+//  }
+
+
+//  console.log("according to your scores, your grades was:" , grades);
